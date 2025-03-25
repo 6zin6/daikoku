e2a8e11a7c070bbd151295e118c32c390e570984
Zin <t.kawaguchi0907@gmail.com>
2025-03-25 12:51:41 +0900

    providers.tsxの型定義を見直しました

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import React, { ReactNode } from 'react';
 import { SessionProvider } from "next-auth/react";
 
-export function AuthProvider({ children }) {
+export function AuthProvider({ children }: { children: ReactNode}) {
   return <SessionProvider>{children}</SessionProvider>;
 }
